88c8ad51279a4b3fe23b9c251192e755efcef029
Yaroslav Sadin <ysadin@synopsys.com>
2019-05-31 18:24:05 +0300

    cpp_red_belt (week_6): set expected complexities

--- cpp_red_belt/week_6/starter_files/search_server.cpp
+++ cpp_red_belt/week_6/starter_files/search_server.cpp
@@ -6,19 +6,35 @@
 #include <sstream>
 #include <iostream>
 
+/*
+  document_input = DN = 50000
+  document_size = DS = 1000
+  num_unique_words = WN = 10000
+  word_size = WS = 100
+  query_inpt = QN = 500000
+  max_query_words = QW = 10
+*/
+
+// O(DS)
 vector<string> SplitIntoWords(const string& line) {
   istringstream words_input(line);
+  // O(DS)
   return {istream_iterator<string>(words_input), istream_iterator<string>()};
 }
 
+// O(DN*DS)
 SearchServer::SearchServer(istream& document_input) {
+  // O(DN*DS)
   UpdateDocumentBase(document_input);
 }
 
+// O(DN*DS)
 void SearchServer::UpdateDocumentBase(istream& document_input) {
   InvertedIndex new_index;
 
+  // O(DN)
   for (string current_document; getline(document_input, current_document); ) {
+    // O(DS)
     new_index.Add(move(current_document));
   }
 
@@ -28,19 +44,25 @@ void SearchServer::UpdateDocumentBase(istream& document_input) {
 void SearchServer::AddQueriesStream(
   istream& query_input, ostream& search_results_output
 ) {
+  // O(QN*DNlogDN)
   for (string current_query; getline(query_input, current_query); ) {
+    // O(QW)
     const auto words = SplitIntoWords(current_query);
 
     map<size_t, size_t> docid_count;
+    // O(QW)
     for (const auto& word : words) {
+      // O(logWN*logDN*WN)
       for (const size_t docid : index.Lookup(word)) {
         docid_count[docid]++;
       }
     }
 
+    // O(DN)
     vector<pair<size_t, size_t>> search_results(
       docid_count.begin(), docid_count.end()
     );
+    // O(DNlogDN)
     sort(
       begin(search_results),
       end(search_results),
@@ -63,16 +85,20 @@ void SearchServer::AddQueriesStream(
   }
 }
 
+// ~O(DS)
 void InvertedIndex::Add(const string& document) {
-  docs.push_back(document);
+  docs.push_back(document); // Amortized O(1)
 
   const size_t docid = docs.size() - 1;
+  // O(DS)*AmortizedO(1)
   for (const auto& word : SplitIntoWords(document)) {
     index[word].push_back(docid);
   }
 }
 
+// O(logWN)
 list<size_t> InvertedIndex::Lookup(const string& word) const {
+  // O(logWN)
   if (auto it = index.find(word); it != index.end()) {
     return it->second;
   } else {
