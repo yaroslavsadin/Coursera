6bb0f37579ef3cfac1d60a49e4f702e8a85d4501
Yaroslav Sadin <ysadin@synopsys.com>
2019-07-27 16:53:15 +0300

    cpp_brown_belt (week_4) : unique_ptr done

--- cpp_brown_belt/week_4/unique_ptr.cpp
+++ cpp_brown_belt/week_4/unique_ptr.cpp
@@ -17,36 +17,34 @@ private:
     }
   }
 public:
-  UniquePtr() {}
+  UniquePtr() : ptr_(nullptr) {}
   UniquePtr(T * ptr) : ptr_(ptr) {}
   UniquePtr(const UniquePtr&) = delete;
-  UniquePtr(UniquePtr&& other) {
+  UniquePtr(UniquePtr&& other) : ptr_(other.ptr_) {
+    other.ptr_ = nullptr;
+  }
+  UniquePtr& operator = (const UniquePtr&) = delete;
+  UniquePtr& operator = (nullptr_t) {
+    Reset(nullptr);
+    return *this;
+  }
+  UniquePtr& operator = (UniquePtr&& other) {
     if(this != &other) {
       ptr_ = other.ptr_;
       other.ptr_ = nullptr;
     }
-  }
-  UniquePtr& operator = (const UniquePtr&) = delete;
-  UniquePtr& operator = (nullptr_t) {
-    DeletePtr();
-    ptr_ = nullptr;
-  }
-  UniquePtr& operator = (UniquePtr&& other) {
-    if(this != &other) {  
-      ptr_ = other.ptr_;
-      other.ptr_ = nullptr;
-    }
+    return *this;
   }
   ~UniquePtr() {
     DeletePtr();
   }
 
   T& operator * () const {
-    return *ptr_;
+    return *Get();
   }
 
   T * operator -> () const {
-    return ptr_;
+    return Get();
   }
 
   T * Release() {
@@ -56,16 +54,18 @@ public:
   }
 
   void Reset(T * ptr) {
-    if(ptr != ptr_) {
+    if(ptr != ptr_) {  
       DeletePtr();
       ptr_ = ptr;
     }
   }
 
   void Swap(UniquePtr& other) {
-    T* ptr = other.ptr_;
-    other.ptr_ = this->ptr_;
-    this->ptr_ = ptr;
+    if(this != &other) {
+      T* ptr = other.ptr_;
+      other.ptr_ = this->ptr_;
+      this->ptr_ = ptr;
+    }
   }
 
   T * Get() const {
@@ -126,8 +126,70 @@ void TestSelf() {
   UniquePtr<int> ptr(new int(42));
   ptr = move(ptr);
   UniquePtr<int> ptr2(move(ptr));
+  ptr2.Swap(ptr2);
+  ptr = move(ptr2);
+  int lol_ = *ptr.Get();
+  ASSERT_EQUAL(lol_,42);
+  ptr2.Reset(ptr.Get());
+  ASSERT_EQUAL(ptr2.Get(),ptr.Get());
+  ptr2.Reset(ptr2.Get());
+  int* lol = ptr.Release();
+  ptr2.Reset(lol);
   ASSERT_EQUAL(ptr.Get(),static_cast<int*>(nullptr));
   ASSERT_EQUAL(*ptr2.Get(),42);
+}
+
+void TestAll() {
+  {  
+    // Construct an empty ptr
+    UniquePtr<int> ptr;
+  }
+  {
+    // Construct from raw ptr
+    UniquePtr<int> ptr(new int(42));
+  }
+  {
+    // Construct from other
+    UniquePtr<int> ptr;
+    UniquePtr<int> ptr2(move(ptr));
+  }
+  {
+    // Assign to self
+    UniquePtr<int> ptr(new int(42));
+    ptr = move(ptr);
+    ASSERT_EQUAL(*ptr.Get(),42)
+  }
+  {
+    // Swap with self
+    UniquePtr<int> ptr(new int(42));
+    ptr.Swap(ptr);
+    ASSERT_EQUAL(*ptr.Get(),42)
+  }
+  {
+    // Set released to self
+    UniquePtr<int> ptr(new int(42));
+    ptr.Reset(ptr.Release());
+    ptr.Reset(ptr.Get());
+    ASSERT_EQUAL(*ptr.Get(),42);
+  }
+  {
+    // Set released to self
+    UniquePtr<int> ptr(new int(42));
+    UniquePtr<int> ptr2(new int(666));
+    ptr.Swap(ptr2);
+    ASSERT_EQUAL(*ptr.Get(),666);
+    ASSERT_EQUAL(*ptr2.Get(),42);
+  }
+  {
+    // Chaining assignment
+    UniquePtr<int> ptr(new int(42));
+    UniquePtr<int> ptr2(new int(666));
+    UniquePtr<int> ptr3(new int(43));
+    ptr = move(ptr2 = move(ptr3));
+    ASSERT_EQUAL(*ptr,43);
+    ASSERT_EQUAL(ptr2.Get(),static_cast<int*>(nullptr));
+    ASSERT_EQUAL(ptr3.Get(),static_cast<int*>(nullptr));
+  }
 }
 
 int main() {
@@ -135,4 +197,5 @@ int main() {
   RUN_TEST(tr, TestLifetime);
   RUN_TEST(tr, TestGetters);
   RUN_TEST(tr, TestSelf);
+  RUN_TEST(tr, TestAll);
 }
