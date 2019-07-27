2ca525ef3e7cba27446f1dea36897f8d9f4fb17b
Yaroslav Sadin <ysadin@synopsys.com>
2019-07-27 03:17:51 +0300

    cpp_brown_belt (week_4) : unique_ptr unknown signal 6

--- cpp_brown_belt/week_4/unique_ptr.cpp
+++ cpp_brown_belt/week_4/unique_ptr.cpp
@@ -4,32 +4,73 @@
 
 using namespace std;
 
+#define LOG_FUNCTION() cerr << __FUNCTION__ << endl
+
 // Реализуйте шаблон класса UniquePtr
 template <typename T>
 class UniquePtr {
 private:
-  // ???
+  T* ptr_ = nullptr;
+  void DeletePtr() {
+    if (ptr_ != nullptr) {
+      delete ptr_;
+    }
+  }
 public:
-  UniquePtr();
-  UniquePtr(T * ptr);
-  UniquePtr(const UniquePtr&);
-  UniquePtr(UniquePtr&& other);
-  UniquePtr& operator = (const UniquePtr&);
-  UniquePtr& operator = (nullptr_t);
-  UniquePtr& operator = (UniquePtr&& other);
-  ~UniquePtr();
+  UniquePtr() {}
+  UniquePtr(T * ptr) : ptr_(ptr) {}
+  UniquePtr(const UniquePtr&) = delete;
+  UniquePtr(UniquePtr&& other) {
+    if(this != &other) {
+      ptr_ = other.ptr_;
+      other.ptr_ = nullptr;
+    }
+  }
+  UniquePtr& operator = (const UniquePtr&) = delete;
+  UniquePtr& operator = (nullptr_t) {
+    DeletePtr();
+    ptr_ = nullptr;
+  }
+  UniquePtr& operator = (UniquePtr&& other) {
+    if(this != &other) {  
+      ptr_ = other.ptr_;
+      other.ptr_ = nullptr;
+    }
+  }
+  ~UniquePtr() {
+    DeletePtr();
+  }
 
-  T& operator * () const;
+  T& operator * () const {
+    return *ptr_;
+  }
 
-  T * operator -> () const;
+  T * operator -> () const {
+    return ptr_;
+  }
 
-  T * Release();
+  T * Release() {
+    T* ptr = ptr_;
+    ptr_ = nullptr;
+    return ptr;
+  }
 
-  void Reset(T * ptr);
+  void Reset(T * ptr) {
+    if(ptr != ptr_) {
+      DeletePtr();
+      ptr_ = ptr;
+    }
+  }
 
-  void Swap(UniquePtr& other);
+  void Swap(UniquePtr& other) {
+    T* ptr = other.ptr_;
+    other.ptr_ = this->ptr_;
+    this->ptr_ = ptr;
+  }
 
-  T * Get() const;
+  T * Get() const {
+    return ptr_;
+  }
 };
 
 
@@ -81,8 +122,17 @@ void TestGetters() {
   ASSERT_EQUAL(ptr->value, 42);
 }
 
+void TestSelf() {
+  UniquePtr<int> ptr(new int(42));
+  ptr = move(ptr);
+  UniquePtr<int> ptr2(move(ptr));
+  ASSERT_EQUAL(ptr.Get(),static_cast<int*>(nullptr));
+  ASSERT_EQUAL(*ptr2.Get(),42);
+}
+
 int main() {
   TestRunner tr;
   RUN_TEST(tr, TestLifetime);
   RUN_TEST(tr, TestGetters);
+  RUN_TEST(tr, TestSelf);
 }
