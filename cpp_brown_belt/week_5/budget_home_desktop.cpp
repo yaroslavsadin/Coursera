8baf36d66803f8dc43851c9e61165018b9071e2c
Yaroslav Sadin <ysadin@synopsys.com>
2019-09-05 17:24:20 +0300

    cpp_brown_belt (week_5): Date ++ -- enhanced

--- cpp_brown_belt/week_5/budget_home_desktop.cpp
+++ cpp_brown_belt/week_5/budget_home_desktop.cpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <map>
 #include <tuple>
+#include <array>
 #include "test_runner.h"
 
 using namespace std;
@@ -12,72 +13,125 @@ template<typename ItT>
 class Range {
 public:
     Range(ItT begin, ItT end) :
-    begin_(begin), end_(end) {}
+    begin_(begin), end_(end), size_(std::distance(begin_,end_)) {}
     ItT begin() const { return begin_; }
     ItT end() const { return end_; }
+    size_t size() { return size_; }
 private:
     ItT begin_;
     ItT end_;
+    size_t size_;
+};
+
+struct Date {
+
+#define JAN 1
+#define FEB 2
+#define MAR 3
+#define APR 4
+#define MAY 5
+#define JUN 6
+#define JUL 7
+#define AUG 8
+#define SEP 9
+#define OCT 10
+#define NOV 11
+#define DEC 12
+
+    int year; int month; int day;
+    const static array<int,13> month_days_count;
+
+#define DATE_COMP_OP(op)                                                            \
+    friend bool operator op (const Date& lhs, const Date& rhs) {                    \
+        return tie(lhs.year,lhs.month,lhs.day) op tie(rhs.year,rhs.month, rhs.day); \
+    }
+    DATE_COMP_OP(>);
+    DATE_COMP_OP(<);
+    DATE_COMP_OP(>=);
+    DATE_COMP_OP(<=);
+    DATE_COMP_OP(==);
+    DATE_COMP_OP(!=);
+#undef DATE_COMP_OP
+
+    bool IsLeapYear() {
+        bool res = false;
+        if (year%400 == 0) // Exactly divisible by 400 e.g. 1600, 2000
+            res = true;
+        else if (year%100 == 0) // Exactly divisible by 100 and not by 400 e.g. 1900, 2100
+            res = false;
+        else if (year%4 == 0) // Exactly divisible by 4 and neither by 100 nor 400 e.g. 2016, 2020
+            res = true;
+        else // Not divisible by 4 or 100 or 400 e.g. 2017, 2018, 2019
+            res = false;
+        return res;
+    }
+
+    int GetMonthDays() {
+        int days_in_month = month_days_count[month];
+        if(month == FEB && IsLeapYear()) {
+            days_in_month++;
+        }
+        return days_in_month;
+    }
+
+    Date& operator++ ()     // prefix ++
+    {
+        // Do work on this.   (increment your object here)
+        if(++day > GetMonthDays()) {
+            day = 1;
+            if(++month > 12) {
+                ++year;
+                month = JAN;
+            }
+        }
+        return *this;
+    }
+
+    Date  operator++ (int)  // postfix ++
+    {
+        Date result(*this);   // make a copy for result
+        ++(*this);              // Now use the prefix version to do the work
+        return result;          // return the copy (the old) value.
+    }
+
+    Date& operator-- ()     // prefix --
+    {
+        // Do work on this.   (decrement your object here)
+        if(--day < 1) {
+            if(--month < 1) {
+                month = DEC;
+                --year;
+            }
+            day = GetMonthDays();
+        }
+        return *this;
+    }
+
+    Date  operator-- (int)  // postfix --
+    {
+        Date result(*this);   // make a copy for result
+        --(*this);              // Now use the prefix version to do the work
+        return result;          // return the copy (the old) value.
+    }
+};
+
+const array<int,13> Date::month_days_count = {
+    0,  // padding
+    31,
+    28, // Leap year is checked later
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31
 };
 
 class BudgetManager {
-#ifdef _DEBUG
-public:
-#endif
-    struct Date {
-        int year; int month; int day; 
-#define DATE_COMP_OP(op)                                                       \
-friend bool operator op (const Date& lhs, const Date& rhs) {                    \
-    return tie(lhs.year,lhs.month,lhs.day) op tie(rhs.year,rhs.month, rhs.day);  \
-}
-        DATE_COMP_OP(>);
-        DATE_COMP_OP(<);
-        DATE_COMP_OP(>=);
-        DATE_COMP_OP(<=);
-        DATE_COMP_OP(==);
-        DATE_COMP_OP(!=);
-#undef DATE_COMP_OP
-
-        Date& operator++ ()     // prefix ++
-        {
-           // Do work on this.   (increment your object here)
-           if(++this->day > 31) {
-               this->day = 1;
-               if(++this->month > 12) {
-                   this->month = 1;
-                   ++this->year;
-               }
-           }
-           return *this;
-        }
-
-        Date  operator++ (int)  // postfix ++
-        {
-           Date result(*this);   // make a copy for result
-           ++(*this);              // Now use the prefix version to do the work
-           return result;          // return the copy (the old) value.
-        }
-
-        Date& operator-- ()     // prefix --
-        {
-           // Do work on this.   (decrement your object here)
-           if(--this->day < 1) {
-               this->day = 31;
-               if(--this->month < 1) {
-                   this->month = 12;
-                   --this->year;
-               }
-           }
-           return *this;
-        }
-
-        Date  operator-- (int)  // postfix --
-        {
-           Date result(*this);   // make a copy for result
-           --(*this);              // Now use the prefix version to do the work
-           return result;          // return the copy (the old) value.
-        }
-    };
 public:
     BudgetManager() {}
     void ComputeIncome(const Date& from, const Date& to, ostream& os = cout) const;
@@ -100,13 +154,13 @@ public:
             return Range(it_from,it_to);
         } else if (it_from == date_to_balance_.end() && it_to != date_to_balance_.end()) {
             Date insertion{to};
-            while(insertion >= from) {
+            while(insertion > from) {
                 date_to_balance_[--insertion];
             }
             return Range(date_to_balance_.find(from),it_to);
         } else if (it_to == date_to_balance_.end() && it_from != date_to_balance_.end()) {
             Date insertion{from};
-            while(insertion <= from) {
+            while(insertion < to) {
                 date_to_balance_[++insertion];
             }
             return Range(it_from,date_to_balance_.find(to));
@@ -122,53 +176,80 @@ public:
 };
 
 void TestDateOperators() {
-    BudgetManager::Date bigger{.year = 1990, .month = 6, .day = 21};
-    BudgetManager::Date smaller{.year = 1990, .month = 6, .day = 20};
+    Date bigger{.year = 1990, .month = JUN, .day = 21};
+    Date smaller{.year = 1990, .month = JUN, .day = 20};
     ASSERT(smaller < bigger);
     ASSERT(bigger > smaller);
     ASSERT(bigger >= smaller);
     ASSERT(smaller <= bigger);
     ASSERT(smaller != bigger);
-    bigger = {.year = 1990, .month = 6, .day = 20};
+    bigger = {.year = 1990, .month = JUN, .day = 20};
     ASSERT(smaller == bigger);
     ASSERT(smaller == bigger);
 }
 
 void TestDateIncDec() {
-    BudgetManager::Date result{.year = 1990, .month = 12, .day = 30};
-    BudgetManager::Date expected{.year = 1990, .month = 12, .day = 31};
-    ASSERT(++result == expected);
-    result++;
-    expected = BudgetManager::Date{.year = 1991, .month = 1, .day = 1};
-    ASSERT(result == expected);
-    expected = BudgetManager::Date{.year = 1990, .month = 12, .day = 31};
-    ASSERT(--result == expected);
-    result--;
-    expected = BudgetManager::Date{.year = 1990, .month = 12, .day = 30};
-    ASSERT(result == expected);
-    result++;
-    ++result;
-    expected = BudgetManager::Date{.year = 1991, .month = 1, .day = 1};
-    ASSERT(result == expected);
-    result--;
-    --result;
-    expected = BudgetManager::Date{.year = 1990, .month = 12, .day = 30};
-    ASSERT(result == expected);
+    {
+        Date result{.year = 1990, .month = DEC, .day = 30};
+        Date expected{.year = 1990, .month = DEC, .day = 31};
+        ASSERT(++result == expected);
+    }
+    {
+        Date result{.year = 2016, .month = MAR, .day = 1};
+        Date expected{.year = 2016, .month = FEB, .day = 29};
+        ASSERT(--result == expected);
+    }
+    {
+        Date result{.year = 2019, .month = MAR, .day = 1};
+        Date expected{.year = 2019, .month = FEB, .day = 28};
+        ASSERT(--result == expected);
+    }
+    {
+        Date result{.year = 2020, .month = FEB, .day = 28};
+        Date expected{.year = 2020, .month = FEB, .day = 29};
+        ASSERT(++result == expected);
+    }
+    {
+        Date result{.year = 2020, .month = FEB, .day = 29};
+        Date expected{.year = 2020, .month = MAR, .day = 1};
+        ASSERT(++result == expected);
+    }
+    {
+        Date result{.year = 2020, .month = DEC, .day = 31};
+        Date expected{.year = 2021, .month = JAN, .day = 1};
+        ASSERT(++result == expected);
+    }
+    {
+        Date result{.year = 2021, .month = JAN, .day = 1};
+        Date expected{.year = 2020, .month = DEC, .day = 31};
+        ASSERT(--result == expected);
+    }
 }
 
 void TestGetDatesRange() {
     BudgetManager bm;
-    BudgetManager::Date from{.year = 1991, .month = 1, .day = 1};
-    BudgetManager::Date to{.year = 1991, .month = 2, .day = 1};
+    Date from{.year = 1991, .month = JAN, .day = 1};
+    Date to{.year = 1991, .month = FEB, .day = 1};
     auto range = bm.GetDatesRange(from,to);
+    ASSERT_EQUAL(range.size(),31u);
     for(const auto& date : range) {
         ASSERT(date.first == from++);
     }
-    from = {.year = 1991, .month = 1, .day = 1};
+
+    from = {.year = 1991, .month = JAN, .day = 1};
     range = bm.GetDatesRange(from,to);
     for(const auto& date : range) {
         ASSERT(date.first == from++);
     }
+
+    from = {.year = 1991, .month = JAN, .day = 25};
+    to = {.year = 1991, .month = FEB, .day = 15};
+    range = bm.GetDatesRange(from,to);
+    ASSERT_EQUAL(range.size(),21u);
+    for(const auto& date : range) {
+        ASSERT(date.first == from++);
+    }
+
 }
 
 int main(void) {
