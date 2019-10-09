befb8a32021b3ce9cbdf3385ede77b0bfa67a127
Yaroslav Sadin <ysadin@synopsys.com>
2019-10-09 20:09:19 +0300

    cpp_black_belt begin

---
+++ cpp_black_belt/iterator_range.h
@@ -0,0 +1,33 @@
+#pragma once
+
+template <typename Iterator>
+class IteratorRange {
+public:
+  IteratorRange(Iterator begin, Iterator end)
+    : first(begin)
+    , last(end)
+    , size_(distance(first, last))
+  {
+  }
+
+  Iterator begin() const {
+    return first;
+  }
+
+  Iterator end() const {
+    return last;
+  }
+
+  size_t size() const {
+    return size_;
+  }
+
+private:
+  Iterator first, last;
+  size_t size_;
+};
+
+template <typename Iterator>
+IteratorRange<Iterator> MakeRange(Iterator i1, Iterator i2) {
+  return IteratorRange(i1,i2);
+}
