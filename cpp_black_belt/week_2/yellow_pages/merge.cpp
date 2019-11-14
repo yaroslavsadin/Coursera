46464eb6b941391f3f886acf2418194f717a4eca
Yaroslav Sadin <ysadin@synopsys.com>
2019-11-15 02:33:07 +0300

    cpp_black_belt (week_2): YellowPages first ugly working version

--- cpp_black_belt/week_2/yellow_pages/merge.cpp
+++ cpp_black_belt/week_2/yellow_pages/merge.cpp
@@ -2,12 +2,124 @@
 
 #include <algorithm>
 #include <tuple>
+#include "message_differencer.h"
 
 namespace YellowPages {
 
-  Company Merge(const Signals& signals, const Providers&) {
-    // Наивная неправильная реализация: берём все данные из самого первого сигнала, никак не учитываем приоритеты
-    return signals.empty() ? Company() : signals[0].company();
+  Company Merge(const Signals& signals, const Providers& providers) {
+    if(signals.empty()) return Company();
+
+    Company res;
+    // Dealing with Address
+    {
+      Address addr;
+      uint32_t cur_prio = 0;
+      for(const auto& signal : signals) {
+        if(signal.has_company() && signal.company().has_address()) {
+          auto provider_prio = providers.at(signal.provider_id()).priority();
+          if(provider_prio > cur_prio) {
+            cur_prio = provider_prio;
+            addr = signal.company().address();
+          }
+        }
+      }
+      *res.mutable_address() = addr;
+    }
+    // Dealing with names
+    {
+      std::vector<Name> names;
+      std::set<std::string> uniques;
+      uint32_t cur_prio = 0;
+      for(const auto& signal : signals) {
+        if(signal.has_company() && signal.company().names_size()) {
+          auto provider_prio = providers.at(signal.provider_id()).priority();
+          if(provider_prio > cur_prio) {
+            cur_prio = provider_prio;
+            names.clear();
+          }
+          if(provider_prio == cur_prio) {
+            for(const auto& name : signal.company().names()) {
+              std::string check_unique;
+              name.AppendToString(&check_unique);
+              if(uniques.insert(check_unique).second)
+                names.push_back(name);
+            }
+          }
+        }
+      }
+      for(auto& name : names) {
+        *res.add_names() = std::move(name);
+      }
+    }
+    // Dealing with phones
+    {
+      std::vector<Phone> phones;
+      std::set<std::string> uniques;
+      uint32_t cur_prio = 0;
+      for(const auto& signal : signals) {
+        if(signal.has_company() && signal.company().phones_size()) {
+          auto provider_prio = providers.at(signal.provider_id()).priority();
+          if(provider_prio > cur_prio) {
+            cur_prio = provider_prio;
+            phones.clear();
+          }
+          if(provider_prio == cur_prio) {
+            for(const auto& name : signal.company().phones()) {
+              std::string check_unique;
+              name.AppendToString(&check_unique);
+              if(uniques.insert(check_unique).second)
+                phones.push_back(name);
+            }
+          }
+        }
+      }
+      for(auto& name : phones) {
+        *res.add_phones() = std::move(name);
+      }
+    }
+    // Dealing with URLs
+    {
+      std::vector<Url> urls;
+      std::set<std::string> uniques;
+      uint32_t cur_prio = 0;
+      for(const auto& signal : signals) {
+        if(signal.has_company() && signal.company().urls_size()) {
+          auto provider_prio = providers.at(signal.provider_id()).priority();
+          if(provider_prio > cur_prio) {
+            cur_prio = provider_prio;
+            urls.clear();
+          }
+          if(provider_prio == cur_prio) {
+            for(const auto& name : signal.company().urls()) {
+              std::string check_unique;
+              name.AppendToString(&check_unique);
+              if(uniques.insert(check_unique).second)
+                urls.push_back(name);
+            }
+          }
+        }
+      }
+      for(auto& name : urls) {
+        *res.add_urls() = std::move(name);
+      }
+    }
+    // Dealing with WorkingTime
+    {
+      WorkingTime working_time;
+      uint32_t cur_prio = 0;
+      for(const auto& signal : signals) {
+        if(signal.has_company() && signal.company().has_working_time()) {
+          auto provider_prio = providers.at(signal.provider_id()).priority();
+          if(provider_prio > cur_prio) {
+            cur_prio = provider_prio;
+            working_time = signal.company().working_time();
+          }
+        }
+      }
+      *res.mutable_working_time() = working_time;
+    }
+    
+    return std::move(res);
   }
 
 }
